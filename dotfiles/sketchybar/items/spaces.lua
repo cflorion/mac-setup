c9a0ad4be30defa9e7d6fc3b75269401f71517dd
Florion <florion@popina.com>
2026-04-14 13:44:02 +0200

    aerospace+sketchybar: reorganize workspaces, raccourci = initiale app

--- dotfiles/sketchybar/items/spaces.lua
+++ dotfiles/sketchybar/items/spaces.lua
@@ -9,7 +9,8 @@ sbar.add("event", "aerospace_workspace_change")
 local spaces = {}
 
 -- All possible AeroSpace workspaces
-local workspace_names = { "A", "B", "C", "M", "O", "S", "T", "1", "2", "3", "4", "5" }
+-- C=Claude, W=WezTerm, S=Safari, M=Superhuman, N=Slack, O=Obsidian, T=TickTick, I=Calendar
+local workspace_names = { "C", "W", "S", "M", "N", "O", "T", "I", "1", "2", "3", "4", "5" }
 
 for _, ws_name in ipairs(workspace_names) do
   local space = sbar.add("item", "space." .. ws_name, {
